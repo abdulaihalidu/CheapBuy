0e9536711d989b3f873e293dacea54ae3bafc9c6
abdulaihalidu <abdulaihalidu008@gmail.com>
2022-06-22 15:23:41 +0300

    storage medium changed

--- backend/settings.py
+++ backend/settings.py
@@ -191,11 +191,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 AWS_QUERYSTRING_AUTH = False
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'cheapbuy-bucket'
 
-
+# turn debug off in deployment
 if os.getcwd() == '/app':
     DEBUG = False
